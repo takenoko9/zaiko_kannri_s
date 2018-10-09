766d214bc659c56825ab2e1eb418b235be5f2991
takenoko9 <xxgrkcfbps14@docomo.ne.jp>
2018-10-09 16:08:28 +0900

    change

--- zaiko_kannri9/config/routes.rb
+++ zaiko_kannri9/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get '/stock/index' => "stock#index"
   get '/product/resistration' => "product#resistration"
+  post "product/create" => "product#create"
   get "product/:id/edit" => "product#edit"
   post "product/:id/update" => "product#update"
   post "product/:id/destroy" => "product#destroy"
-  post "product/create" => "product#create"
+
   get "home/top" => "home#top"
   get "/home/login" => "home#login"
 
